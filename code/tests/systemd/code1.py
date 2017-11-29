42da08325dd5589c93f47ed7659f86ec0b80f5d7
Gareth Howell <gareth@howell.name>
2017-11-29 17:28:25 +0000

    Logging

--- code/tests/systemd/code1.py
+++ code/tests/systemd/code1.py
@@ -1,8 +1,44 @@
-#! /usr/bin/python
+#!/usr/bin/env python3
 
 import logging
+import sys
+import time
 
-logging.basicConfig(level=logging.DEBUG)
+def main():
+    root_logger = logging.getLogger()
+    root_logger.setLevel("INFO")
+    root_logger.addHandler(SystemdHandler())
 
-log = logging.getLogger("code1")
-log.info("Code1")
+    while True:
+        logging.debug("Debug")
+        logging.info("Info")
+        logging.warning("Warning")
+        time.sleep(60)
+
+class SystemdHandler(logging.Handler):
+    # http://0pointer.de/public/systemd-man/sd-daemon.html
+    PREFIX = {
+        # EMERG <0>
+        # ALERT <1>
+        logging.CRITICAL: "<2>",
+        logging.ERROR: "<3>",
+        logging.WARNING: "<4>",
+        # NOTICE <5>
+        logging.INFO: "<6>",
+        logging.DEBUG: "<7>",
+        logging.NOTSET: "<7>"
+    }
+
+    def __init__(self, stream=sys.stdout):
+        self.stream = stream
+        logging.Handler.__init__(self)
+
+    def emit(self, record):
+        try:
+            msg = self.PREFIX[record.levelno] + self.format(record) + "\n"
+            self.stream.write(msg)
+            self.stream.flush()
+        except Exception:
+            self.handleError(record)
+
+main()
