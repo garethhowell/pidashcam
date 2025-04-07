9882f7f7df3f6cfc00516a66f273c4b36a808596
Gareth Howell <gareth.howell@gmail.com>
2025-04-07 10:29:33 +0100

    Conversion to use PDM as a project manager

--- src/pidashcam/myqueue.py
+++ src/pidashcam/myqueue.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python
 
-import Queue
+import queue
 
-class MyQueue(Queue.Queue):
+class MyQueue(queue.Queue):
   '''
   A custom queue subclass that provides a :meth: `clear` method.
   '''
