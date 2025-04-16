da35a7d1270aeefd7f6ee825af899b792eb54dcc
Gareth Howell <gareth.howell@gmail.com>
2025-04-16 18:56:08 +0100

    Add comments

--- src/pidashcam/config.py
+++ src/pidashcam/config.py
@@ -1,20 +1,41 @@
+# Which camera are we capturing from?
+# If installed, the picamera will be source 0
 src = 0
+
+# How far ahead of the point at which we choose to save
+# do we need to keep so it can be flushed (seconds)
 pre_record = 30
+
+# How long after the point at which we choose to save
+# do we need to keep saving (seconds)
 post_record = 30
+
+# define the size of the video
 width = 1440
 height = 1200
+
+# Define the GPIO pin used to trigger saving (BCM pins, not connector)
 button_A = 23
+
+# Ditto for the button that defines whether or not we are recording
 button_B = 24
+
+# Define the GPIO pin to which the "Recording" LED is connected
 LED_1 = 25
+
+# Where do we save the video files
 dest_dir = '/tmp'
+
+# What format do we save in
 video_format = 'h264'
+
+# Log level (set to error in production)
 log_level = 'debug'
+
+# Do we need to transform the image:
+# e.g. because the camera is upside down
 vflip = False
 hflip = False
 
-# specify the desired video length (seconds).
-# this will determine the number of frames to be
-# kept in each camera buffer.  A 10 second clip
-# will capture 300 frames (10s * 30fps)
-video_length = 5 * 60
-buffer_size = pre_record + video_length + post_record
+# Computed - DO NOT CHANGE
+buffer_size = pre_record + post_record
